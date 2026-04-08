4038ad6ebb90a35ff8f932ba350bd833d39f810c
Sneh17029 <60146980+Sneh17029@users.noreply.github.com>
2026-04-08 18:09:26 +0530

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, m, piles):
+        s = 0
+        for i in range(len(piles)):
+            s += (piles[i]+m-1)//m
+        return s
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        while l<r:
+            m = l + (r-l)//2
+            s = self.search(m, piles)
+            if s <= h:
+                r = m
+            else:
+                l = m + 1
+        return l
